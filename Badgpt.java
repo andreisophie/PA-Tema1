6282229b5c38726606e9963d7d40798663c7db85
andreisophie <andreisophie@yahoo.com>
2023-04-08 17:02:33 +0300

    Added skels

---
+++ Badgpt.java
@@ -0,0 +1,7 @@
+public class Badgpt {
+
+
+    public static void main(String[] args) {
+        System.out.println("I'm alive!");
+    }
+}
---
+++ Feribot.java
@@ -0,0 +1,5 @@
+public class Feribot {
+    public static void main(String[] args) {
+        return;
+    }
+}
